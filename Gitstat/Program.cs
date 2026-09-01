039db305f477fbce5541335df1c40dd4421c5ff1
pr1ceadv <dutkevychandriy@gmail.com>
2026-09-01 18:39:16 +0300

    feat: add json deserialization

--- Gitstat/Program.cs
+++ Gitstat/Program.cs
@@ -3,33 +3,79 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
 
+
+public class Author
+{   
+    
+    public string login { get; set; }
+   
+}
+public class CommitAuthor
+{
+    public string name { get; set; }
+    public string email { get; set; }
+    public string  date { get; set; }
+}
+
+public class Commit
+{
+    public CommitAuthor  author { get; set; }
+    public string  message { get; set; }
+    
+}
+
+public class Data
+{
+    public string Sha { get; set; }
+    public Commit commit { get; set; }
+    public Author author { get; set; }
+    
+    public override string ToString()
+    {
+        return $"Sha: {Sha}\nAuthor: {author?.login}";
+    }
+}
 class Program
 {   
-    private static HttpClient sharedClient = new()
+    private static HttpClient _sharedClient = new()
     {
-        BaseAddress = new Uri("https://api.github.com/repos/dotnet/runtime/commits"),
+        BaseAddress = new Uri("https://api.github.com/"),
     };
     
 
-    static async Task GetData(HttpClient client)
+    static async Task<string> GetData(HttpClient client)
     {
+        string jsonResponse = "";
+        
         try
-        {
-            HttpResponseMessage response = await client.GetAsync("/");
+        {   
+            string BaseUrl = client.BaseAddress.ToString();
+            string url = $"{BaseUrl}repos/dotnet/runtime/commits";
+            HttpResponseMessage response = await client.GetAsync(url);
             response.EnsureSuccessStatusCode();
 
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"{jsonResponse}\n");
+            jsonResponse = await response.Content.ReadAsStringAsync();
+            
         }
         catch (HttpRequestException e)
         {
             Console.WriteLine($"{e.Message}");
         }
+        return jsonResponse;
     }
     
     static async Task Main(string[] args)
-    {
-        sharedClient.DefaultRequestHeaders.Add("User-Agent", "dotnet");
-        await GetData(sharedClient);
+    {   
+        _sharedClient.DefaultRequestHeaders.Add("User-Agent", "dotnet");
+        
+        var jsonResponse = await GetData(_sharedClient);
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        
+        Data[]? data = JsonSerializer.Deserialize<Data[]>(jsonResponse, options);
+        Console.WriteLine(data.Length);
     }
 }
