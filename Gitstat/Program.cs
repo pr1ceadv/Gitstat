abe47f77744286a6b4617ab7ae04f828d9a38387
pr1ceadv <dutkevychandriy@gmail.com>
2026-09-01 17:37:24 +0300

    chore: initial project setup

---
+++ Gitstat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+class Program
+{   
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("https://api.github.com/repos/dotnet/runtime/commits"),
+    };
+    
+
+    static async Task GetData(HttpClient client)
+    {
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"{e.Message}");
+        }
+    }
+    
+    static async Task Main(string[] args)
+    {
+        sharedClient.DefaultRequestHeaders.Add("User-Agent", "dotnet");
+        await GetData(sharedClient);
+    }
+}
